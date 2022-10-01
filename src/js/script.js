cd29363e68d78a32e352f7bb33f5b5c7613ec299
Dima zagoruiko <dima.zagorujko@mail.ru>
2022-10-01 20:48:00 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg" ></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg" ></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }
+            }
+        ]
+      });
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass
+          ('catalog__content_active').eq($(this).index()).addClass
+          ('catalog__content_active');
+      });
+  });
+
+  var slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false , 
+    nav: false,
+    responsive:{
+        768: {
+        edgePadding: 20,
+        gutter: 20,
+        nav: false,
+        items: 1,
+
+        }
+    }
+    
+  });
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
